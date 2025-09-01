a9e97b0d53ae478c575d9c1ca8aa53233f6812fb
Larro123 <slarrosa@mail.austral.edu.ar>
2025-09-01 12:44:02 -0300

    Add files via upload

--- slice_advanced.py
+++ slice_advanced.py
@@ -1,14 +1,7 @@
 def slice_advanced():
-    # Código a implementar utilizando input.
-
-# Para verificar este ejercicio ejecutar el comando
-# `pytest tp3_slice_advanced_test.py` o `python tp3_slice_advanced_test.py`
-
     nombre = input("Ingrese un nombre: ")
     print("contiene a: ", "a" in nombre)
     print("contiene e: ", "e" in nombre)
     print("contiene i: ", "i" in nombre)
     print("contiene o: ", "o" in nombre)
     print("contiene u: ", "u" in nombre)
-
-slice_advanced()
